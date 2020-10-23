e0f375662df311017d15c27c148580ece40ebd03
Merces-dev <giovanitjs013@gmail.com>
2020-10-23 03:18:05 -0300

    Nyous-React - Comecando

--- README.md
+++
@@ -1,3 +0,0 @@
-# SenaiExerciciosAula-Javascript-
-## Exercicios e Aulas referentes ao curso de Desenvolvimento de sistemas no SENAI
-### Javascript e React.js
---
+++ ReactAula5/nyous-react/src/pages/login/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Menu from '../../components/menu'
+import Rodape from '../../components/rodape'
+
+
+const Login = ()=> {
+        return(
+            <div>
+                <Menu/>
+                <h1>
+                    Login
+                </h1>
+                <Rodape/>
+            </div>
+        )
+}
+export default Login
